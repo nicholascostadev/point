85b91f5d7e5e6aa151eb7a111b0e4196b933f943
Nicholas Costa <nicholascostadev@gmail.com>
2023-04-24 20:29:25 -0300

    refactor: references page to me easier to add items

--- src/app/references/page.tsx
+++ src/app/references/page.tsx
@@ -1,97 +1,38 @@
-const references = [
-    {
-        name: "Linear",
-        url: "https://linear.app",
-    },
-    {
-        name: "Clerk",
-        url: "https://clerk.com",
-    },
-];
-
-const designInspirations = [
-    {
-        name: "Linear",
-        desc: " - Landing Page",
-        url: "https://linear.app",
-    },
-    {
-        name: "Vercel",
-        desc: " - Landing Page & Pricing Page",
-        url: "https://vercel.com",
-    },
-    {
-        name: "Figma",
-        desc: " - Landing Page",
-        url: "https://figma.com",
-    },
-];
+import { sections } from "./referencesData";
 
 export default function References() {
     return (
         <main className="w-layout-base max-w-full flex flex-col md:flex-row gap-4 px-4 md:px-8 py-8 mx-auto">
-            <section
-                aria-labelledby="references"
-                className="flex flex-col gap-4"
-            >
-                <h2 className="text-4xl" id="references">
-                    References
-                </h2>
+            {sections.map((section) => (
+                <section
+                    key={section.id}
+                    aria-labelledby={section.id}
+                    className="flex flex-col gap-4"
+                >
+                    <h2 className="text-4xl" id="references">
+                        {section.name}
+                    </h2>
 
-                <p>
-                    This project makes use of some awesome projects, all of them
-                    are listed below, please give them some kudos.
-                </p>
-                <ol className="list-decimal pl-5">
-                    {references.map((reference) => (
-                        <li
-                            key={reference.url}
-                            className="underline-offset-4 w-auto"
-                        >
-                            <a
-                                href={reference.url}
-                                className="group dark:text-gray-300 dark:hover:text-gray-100 transition-colors"
+                    <p>{section.description}</p>
+                    <ol className="list-decimal pl-5">
+                        {section.data.map((sectionData) => (
+                            <li
+                                key={sectionData.url}
+                                className="underline-offset-4 w-auto"
                             >
-                                <span className="underline decoration-dotted group-hover:decoration-dashed">
-                                    {reference.name}
-                                </span>
-                            </a>
-                        </li>
-                    ))}
-                </ol>
-            </section>
-            <section
-                aria-labelledby="design-inspirations"
-                className="flex flex-col gap-4"
-            >
-                <h2 className="text-4xl" id="design-inspirations">
-                    Design inspirations
-                </h2>
-
-                <p>
-                    I used some design inspirations from these awesome projects
-                    to make this project look good, give them a try if you
-                    haven&apos;t.
-                </p>
-                <ol className="list-decimal pl-5">
-                    {designInspirations.map((inspiration) => (
-                        <li
-                            key={inspiration.url}
-                            className="underline-offset-4 w-auto"
-                        >
-                            <a
-                                href={inspiration.url}
-                                className="group dark:text-gray-300 dark:hover:text-gray-100 transition-colors"
-                            >
-                                <span className="underline decoration-dotted group-hover:decoration-dashed">
-                                    {inspiration.name}
-                                </span>
-                                {inspiration.desc}
-                            </a>
-                        </li>
-                    ))}
-                </ol>
-            </section>
+                                <a
+                                    href={sectionData.url}
+                                    className="group dark:text-gray-300 dark:hover:text-gray-100 transition-colors"
+                                >
+                                    <span className="underline decoration-dotted group-hover:decoration-dashed">
+                                        {sectionData.name}
+                                    </span>
+                                </a>
+                            </li>
+                        ))}
+                    </ol>
+                </section>
+            ))}
         </main>
     );
 }
---
+++ src/app/references/referencesData.ts
@@ -0,0 +1,45 @@
+const references = [
+    {
+        name: "Linear",
+        url: "https://linear.app",
+    },
+    {
+        name: "Clerk",
+        url: "https://clerk.com",
+    },
+];
+
+const designInspirations = [
+    {
+        name: "Linear",
+        desc: " - Landing Page",
+        url: "https://linear.app",
+    },
+    {
+        name: "Vercel",
+        desc: " - Landing Page & Pricing Page",
+        url: "https://vercel.com",
+    },
+    {
+        name: "Figma",
+        desc: " - Landing Page",
+        url: "https://figma.com",
+    },
+];
+
+export const sections = [
+    {
+        name: "References",
+        id: "references",
+        description:
+            "This project makes use of some awesome projects, all of them are listed below, make sure to give them some kudos.",
+        data: references,
+    },
+    {
+        name: "Design inspirations",
+        id: "design-inspirations",
+        description:
+            "I used some design inspirations from these awesome projects to make this project look good, give them a try if you haven't.",
+        data: designInspirations,
+    },
+];
