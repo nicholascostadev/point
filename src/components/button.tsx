486cb5f3cc41faf77deff6b3fc0f5fbf6eec4a01
Nicholas Costa <nicholascostadev@gmail.com>
2023-04-27 00:40:16 -0300

    refactor: forward button ref

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,6 @@
 import { cl } from "@/utils/cl";
 import Link, { LinkProps } from "next/link";
+import { forwardRef } from "react";
 
 type ButtonProps =
     | ({
@@ -10,16 +11,32 @@ type ButtonProps =
           as: "button";
       } & React.ComponentPropsWithoutRef<"button">);
 
-export function Button({ className, ...rest }: ButtonProps) {
-    const buttonClasses =
-        "flex items-center justify-center gap-2 text-lg hover:bg-gray-200 active:bg-gray-300 dark:bg-gray-900 dark:hover:bg-gray-800 dark:active:bg-gray-700 transition-colors py-2 px-6 rounded-full dark:text-gray-200 [&_svg]:dark:fill-gray-200 [&_svg]:fill-gray-950";
+export const Button = forwardRef<any, ButtonProps>(
+    ({ className, ...rest }, ref) => {
+        const buttonClasses =
+            "flex items-center justify-center gap-2 text-lg hover:bg-gray-200 active:bg-gray-300 dark:bg-gray-900 dark:hover:bg-gray-800 dark:active:bg-gray-700 transition-colors py-2 px-6 rounded-full dark:text-gray-200 [&_svg]:dark:fill-gray-200 [&_svg]:fill-gray-950";
 
-    if (rest.as === "a") {
-        // as is a reserved prop inside Link component
-        const { as, ...props } = rest;
+        if (rest.as === "a") {
+            // as is a reserved prop inside Link component
+            const { as, ...props } = rest;
 
-        return <Link className={cl(buttonClasses, className)} {...props} />;
+            return (
+                <Link
+                    ref={ref}
+                    className={cl(buttonClasses, className)}
+                    {...props}
+                />
+            );
+        }
+
+        return (
+            <button
+                ref={ref}
+                className={cl(buttonClasses, className)}
+                {...rest}
+            />
+        );
     }
+);
 
-    return <button className={cl(buttonClasses, className)} {...rest} />;
-}
+Button.displayName = "Button";
