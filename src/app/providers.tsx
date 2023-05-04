c1f5b7a840edc9eee723aeac138fa34453823cb9
Nicholas Costa <nicholascostadev@gmail.com>
2023-05-04 14:33:37 -0300

    feat: add <Toaster />

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
+            <Toaster />
             <ThemeProvider attribute="class" enableColorScheme enableSystem>
                 {children}
             </ThemeProvider>
