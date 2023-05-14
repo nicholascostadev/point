250087451e804225c38d4ce94431e54841c05dca
Nicholas Costa <nicholascostadev@gmail.com>
2023-05-14 16:34:41 -0300

    feat: add label for image input

--- src/app/(common)/dashboard/addProjectForm.tsx
+++ src/app/(common)/dashboard/addProjectForm.tsx
@@ -154,71 +154,79 @@ export function AddProjectForm({ closeModal }: AddProjectFormProps) {
                     </small>
                 )}
             </fieldset>
-            <div
-                className={cl(
-                    "mt-2 flex flex-col items-center justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10",
-                    isDragActive ? "bg-blue-600/10" : ""
-                )}
-            >
-                {preview && (
-                    <div className="relative h-72 w-full">
-                        <Image
-                            src={preview}
-                            alt=""
-                            fill
-                            className="object-contain"
-                        />
-                    </div>
-                )}
-                {!files[0] && (
-                    <div className="text-center" {...getRootProps()}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            className="mx-auto h-12 w-12 text-gray-400"
-                        >
-                            <path
-                                fill="currentColor"
-                                fillRule="evenodd"
-                                d="M5.5 17a4.5 4.5 0 0 1-1.44-8.765a4.5 4.5 0 0 1 8.302-3.046a3.5 3.5 0 0 1 4.504 4.272A4 4 0 0 1 15 17H5.5Zm3.75-2.75a.75.75 0 0 0 1.5 0V9.66l1.95 2.1a.75.75 0 1 0 1.1-1.02l-3.25-3.5a.75.75 0 0 0-1.1 0l-3.25 3.5a.75.75 0 1 0 1.1 1.02l1.95-2.1v4.59Z"
-                                clipRule="evenodd"
-                            ></path>
-                        </svg>
-                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
+            <fieldset>
+                <label htmlFor="file-upload">Upload an example image</label>
+                <div
+                    className={cl(
+                        "mt-2 flex flex-col items-center justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10",
+                        isDragActive ? "bg-blue-600/10" : ""
+                    )}
+                >
+                    {preview && (
+                        <div className="relative h-72 w-full">
+                            <Image
+                                src={preview}
+                                alt=""
+                                fill
+                                className="object-contain"
+                            />
+                        </div>
+                    )}
+                    {!files[0] && (
+                        <div className="text-center" {...getRootProps()}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                className="mx-auto h-12 w-12 text-gray-400"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    fillRule="evenodd"
+                                    d="M5.5 17a4.5 4.5 0 0 1-1.44-8.765a4.5 4.5 0 0 1 8.302-3.046a3.5 3.5 0 0 1 4.504 4.272A4 4 0 0 1 15 17H5.5Zm3.75-2.75a.75.75 0 0 0 1.5 0V9.66l1.95 2.1a.75.75 0 1 0 1.1-1.02l-3.25-3.5a.75.75 0 0 0-1.1 0l-3.25 3.5a.75.75 0 1 0 1.1 1.02l1.95-2.1v4.59Z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                            <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                <label
+                                    htmlFor="file-upload"
+                                    className="relative cursor-pointer font-semibold text-blue-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 hover:text-blue-500"
+                                >
+                                    {`Choose files`}
+                                    <input
+                                        id="file-upload"
+                                        className="sr-only"
+                                        {...getInputProps()}
+                                    />
+                                </label>
+                                <p className="pl-1">{`or drag and drop`}</p>
+                            </div>
+                            <div className="h-[1.25rem]">
+                                {fileTypes && (
+                                    <p className="text-xs leading-5 text-gray-600">
+                                        {`${fileTypes.join(", ")}`}{" "}
+                                        {maxSize && `up to ${maxSize}`}
+                                    </p>
+                                )}
+                            </div>
+                        </div>
+                    )}
+                    {files[0] && (
+                        <div {...getRootProps()}>
                             <label
                                 htmlFor="file-upload"
                                 className="relative cursor-pointer font-semibold text-blue-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 hover:text-blue-500"
                             >
-                                {`Choose files`}
+                                {`Choose another file`}
                                 <input
                                     className="sr-only"
+                                    id="file-upload"
                                     {...getInputProps()}
                                 />
                             </label>
-                            <p className="pl-1">{`or drag and drop`}</p>
                         </div>
-                        <div className="h-[1.25rem]">
-                            {fileTypes && (
-                                <p className="text-xs leading-5 text-gray-600">
-                                    {`${fileTypes.join(", ")}`}{" "}
-                                    {maxSize && `up to ${maxSize}`}
-                                </p>
-                            )}
-                        </div>
-                    </div>
-                )}
-                {files[0] && (
-                    <div {...getRootProps()}>
-                        <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer font-semibold text-blue-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600 focus-within:ring-offset-2 hover:text-blue-500"
-                        >
-                            {`Choose another file`}
-                            <input className="sr-only" {...getInputProps()} />
-                        </label>
-                    </div>
-                )}
-            </div>
+                    )}
+                </div>
+            </fieldset>
 
             <Button as="button" type="submit" disabled={isSubmitting}>
                 {isSubmitting || isLoading ? (
